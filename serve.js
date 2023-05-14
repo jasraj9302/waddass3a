9b6d6f33926c81dc96db47f957cd4c59e0dbaeb1
Jasraj <jasrajpatil123@gmail.com>
2023-05-15 04:01:08 +0530

    Initial commit

---
+++ serve.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const abc = express();
+
+const path = require("path")
+
+const PORT = 8000
+
+abc.use(express.static('public'));
+
+abc.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+abc.listen(PORT, function(){
+
+ console.log(`Server listening on port::${PORT}`);
+
+});
+
+
+
+
+//npm init -y
+//npm install express
+//node server.js
